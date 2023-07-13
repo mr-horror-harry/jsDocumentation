a0002d557f27400fbab9b1231da3ff4567de230e
M A HARISH J <mr.horror3737@gmail.com>
2023-07-13 10:27:19 +0530

    output

---
+++ 7output.js
@@ -0,0 +1,11 @@
+function output(){
+
+    alert("Im Alert Mssg!")         // contains just the "ok"
+
+    prompt("Im prompt say Hello!", )        // contains an user input field
+
+    confirm("Im a model")           // contains "ok" & "cancel" option
+
+    console.log("Console output")       // console logging
+
+}
