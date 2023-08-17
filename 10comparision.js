7b32515d996f73911c1ff9599d8cdf70945d322f
M A HARISH J <mr.horror3737@gmail.com>
2023-08-17 20:56:48 +0530

    comp

---
+++ 10comparision.js
@@ -0,0 +1,21 @@
+// comparision ==> boolean
+
+console.log(2>=1, 2==1, 2>='1', true==1, ''==false);        // comprision & typecast
+
+let res = 1=='1'    // Boolean variable
+console.log(res)    
+
+console.log('Z' > 'a', 'Hello' == 'hello', 'Hell' < 'Hello')      // ascii value & ascii weightage
+console.log('Hello ' > 'Hello', 'Hello ' == 'Hello');        // output --> true, false : strlen of space
+
+// Strict Equality
+console.log('1'==='1', 1==='1')
+
+// Special null
+console.log(null==0, null>0, null>=0)
+
+// undefined
+console.log(undefined>=0, undefined==0);
+
+// note: undefined & null points each other
+console.log(undefined==null);
